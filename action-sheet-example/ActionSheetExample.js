8e9c8c810054f3b79b4587a2b9c0e4e1f47f71ad
Jack Lam <jacklam718@gmail.com>
2017-03-07 23:11:23 +0800

    update example app

--- action-sheet-example/ActionSheetExample.js
+++ action-sheet-example/ActionSheetExample.js
@@ -1,60 +1,114 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-
-import { ActionSheet, ActionSheetItem, Separator } from 'react-native-action-sheet-component';
-
+import { EvilIcons, Ionicons } from '@exponent/vector-icons';
+import ActionSheet, { ActionSheetItem } from 'react-native-action-sheet-component';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: 'center',
+    alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    padding: 50,
+  },
+  buttonText: {
+    fontSize: 16,
+  },
 });
+
+const checkedIcon = <Ionicons name="ios-checkmark-outline" size={30} />;
 
 export default class ActionSheetExample extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      selectedItems: ['item2'],
+    };
   }
 
-  showActionSheet = () => {
-    this.actionSheet.show();
+  showTopActionSheet = () => {
+    this.topActionSheet.show();
   }
 
-  hideActionSheet = () => {
-    this.actionSheet.hide();
+  showBottomActionSheet = () => {
+    this.bottomActionSheet.show();
+  }
+
+  onChange = (value, index, values) => {
+    console.log(values);
+    this.setState({ selectedItems: values });
+  }
+
+  onItemPress = (value) => {
+    console.log('Press: value -> ', value);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this.showActionSheet}>
-          <Text>
-            Show Action Sheet
+        <TouchableOpacity onPress={this.showTopActionSheet} style={styles.button}>
+          <Text style={styles.buttonText}>
+            Show Top Action Sheet
           </Text>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={this.hideActionSheet}>
-          <Text>
-            Hide Action Sheet
+        <TouchableOpacity onPress={this.showBottomActionSheet} style={styles.button}>
+          <Text style={styles.buttonText}>
+            Show Bottom Action Sheet (Multiple Select)
           </Text>
         </TouchableOpacity>
 
         <ActionSheet
-          ref={(actionSheet) => { this.actionSheet = actionSheet; }}
+          ref={(actionSheet) => { this.topActionSheet = actionSheet; }}
+          position="top"
+          onChange={this.onChange}
         >
           <ActionSheetItem
-            text="Hello"
-            selected
-            // icon={require('./src/img/checkmark.png')}
+            text="Github"
+            value="item1"
+            selectedIcon={checkedIcon}
+            icon={
+              <EvilIcons name="sc-github" size={42} />
+            }
+            onPress={this.onItemPress}
           />
-          <Separator />
           <ActionSheetItem
-            text="Hello"
-            selected
-            // icon={require('./src/img/checkmark.png')}
+            text="Facebook"
+            value="item2"
+            selectedIcon={checkedIcon}
+            icon={
+              <EvilIcons name="sc-facebook" color="#4363A2" size={42} />
+            }
+            onPress={this.onItemPress}
           />
-          <Separator />
+        </ActionSheet>
+
+        <ActionSheet
+          ref={(actionSheet) => { this.bottomActionSheet = actionSheet; }}
+          position="bottom"
+          onChange={this.onChange}
+          multiple
+        >
+          <ActionSheetItem
+            text="Share"
+            value="item1"
+            selectedIcon={checkedIcon}
+            icon={
+              <EvilIcons name="share-google" color="#4B8FF6" size={42} />
+            }
+            onPress={this.onItemPress}
+          />
+          <ActionSheetItem
+            text="Delete"
+            value="item2"
+            selectedIcon={checkedIcon}
+            icon={
+              <EvilIcons name="trash" color="#fa4b4b" size={42} />
+            }
+            onPress={this.onItemPress}
+          />
         </ActionSheet>
       </View>
     );
