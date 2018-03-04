13d6145b30c1f7eb9cea710a73588b17574caf6b
jacklam718 <jacklam718@gmail.com>
2018-03-05 00:34:15 +0800

    New build and Update version

--- dist/ActionSheetItem.js
+++ dist/ActionSheetItem.js
@@ -48,6 +48,7 @@ justifyContent:'center'}});
 
 
 
+
 var defaultProps={
 value:null,
 icon:null,
@@ -56,7 +57,8 @@ selectedIcon:DEFAULT_SELECTED_ICON,
 selected:false,
 showSelectedIcon:true,
 onPress:function onPress(){},
-style:null};
+style:null,
+textStyle:null};
 
 
 function ActionSheetItem(_ref)
@@ -69,20 +71,21 @@ function ActionSheetItem(_ref)
 
 
 
-{var text=_ref.text,value=_ref.value,index=_ref.index,onPress=_ref.onPress,style=_ref.style,icon=_ref.icon,selectedIcon=_ref.selectedIcon,selected=_ref.selected,showSelectedIcon=_ref.showSelectedIcon;
+
+{var text=_ref.text,value=_ref.value,index=_ref.index,onPress=_ref.onPress,style=_ref.style,icon=_ref.icon,selectedIcon=_ref.selectedIcon,selected=_ref.selected,showSelectedIcon=_ref.showSelectedIcon,textStyle=_ref.textStyle;
 var iconOnSelected=void 0;
 var itemIcon=void 0;
 
 if(selected&&showSelectedIcon){
 if(['number','string'].includes(typeof selectedIcon)){
-iconOnSelected=_react2.default.createElement(_reactNative.Image,{source:selectedIcon,style:styles.selectedIcon,__source:{fileName:_jsxFileName,lineNumber:78}});
+iconOnSelected=_react2.default.createElement(_reactNative.Image,{source:selectedIcon,style:styles.selectedIcon,__source:{fileName:_jsxFileName,lineNumber:81}});
 }else{
 iconOnSelected=selectedIcon;
 }
 }
 
 if(['number','string'].includes(typeof icon)){
-itemIcon=_react2.default.createElement(_reactNative.Image,{source:icon,style:styles.itemIcon,__source:{fileName:_jsxFileName,lineNumber:85}});
+itemIcon=_react2.default.createElement(_reactNative.Image,{source:icon,style:styles.itemIcon,__source:{fileName:_jsxFileName,lineNumber:88}});
 }else{
 itemIcon=icon;
 }
@@ -92,22 +95,22 @@ _react2.default.createElement(_reactNative.TouchableHighlight,{
 onPress:function(_onPress){function onPress(){return _onPress.apply(this,arguments);}onPress.toString=function(){return _onPress.toString();};return onPress;}(function(){
 onPress(value,index);
 }),
-underlayColor:'#EDEDED',__source:{fileName:_jsxFileName,lineNumber:91}},
+underlayColor:'#EDEDED',__source:{fileName:_jsxFileName,lineNumber:94}},
 
-_react2.default.createElement(_reactNative.View,{style:[styles.container,style],__source:{fileName:_jsxFileName,lineNumber:97}},
-_react2.default.createElement(_reactNative.View,{style:styles.itemContent,__source:{fileName:_jsxFileName,lineNumber:98}},
+_react2.default.createElement(_reactNative.View,{style:[styles.container,style],__source:{fileName:_jsxFileName,lineNumber:100}},
+_react2.default.createElement(_reactNative.View,{style:styles.itemContent,__source:{fileName:_jsxFileName,lineNumber:101}},
 itemIcon,
-_react2.default.createElement(_reactNative.Text,{style:styles.text,__source:{fileName:_jsxFileName,lineNumber:100}},
+_react2.default.createElement(_reactNative.Text,{style:[styles.text,textStyle],__source:{fileName:_jsxFileName,lineNumber:103}},
 text)),
 
 
-_react2.default.createElement(_reactNative.View,{style:styles.itemContent,__source:{fileName:_jsxFileName,lineNumber:104}},
+_react2.default.createElement(_reactNative.View,{style:styles.itemContent,__source:{fileName:_jsxFileName,lineNumber:107}},
 iconOnSelected))));
 
 
 
 
-}ActionSheetItem.propTypes={text:require('prop-types').any.isRequired,value:require('prop-types').any,index:require('prop-types').number,icon:require('prop-types').any,selectedIcon:require('prop-types').any,selected:require('prop-types').bool,showSelectedIcon:require('prop-types').bool,onPress:require('prop-types').func,style:require('prop-types').any};
+}ActionSheetItem.propTypes={text:require('prop-types').any.isRequired,value:require('prop-types').any,index:require('prop-types').number,icon:require('prop-types').any,selectedIcon:require('prop-types').any,selected:require('prop-types').bool,showSelectedIcon:require('prop-types').bool,onPress:require('prop-types').func,style:require('prop-types').any,textStyle:require('prop-types').any};
 
 ActionSheetItem.defaultProps=defaultProps;exports.default=
 
